a2b2dd4f7652a2492796b2e767864a624f71419c
uday-g-ithub15 <mudayhasan@gmail.com>
2022-05-08 13:49:43 +0600

    'Get-single-data'

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express()
@@ -6,6 +7,33 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpw4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+    const run = async () => {
+        try{
+            await client.connect();
+            const productsCollection =  client.db('warehouse').collection('warehouseproduct') ;
+
+            app.get('/warehouseproducts', async (req, res) => {
+                const query = {};
+                const cursor = productsCollection.find(query)
+                const products = await cursor.toArray();
+                res.send(products)
+            })
+            app.get('/warehouseproducts/:id', async(req, res) => {
+                const id = req.params.id;
+                const query = {_id:ObjectId(id)}
+                const singleProduct = await productsCollection.findOne(query)
+                res.send(singleProduct)
+            })
+
+        }
+        finally{}
+    }
+    run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Runnign')
