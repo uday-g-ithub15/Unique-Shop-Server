36ee29dcc00bffc616a325588f2fc2ece493affc
uday-g-ithub15 <mudayhasan@gmail.com>
2022-06-02 16:26:31 +0600

    Checking

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 app.use(cors())
@@ -12,68 +12,68 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpw4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-    const run = async () => {
-        try{
-            await client.connect();
-            const productsCollection =  client.db('warehouse').collection('warehouseproduct') ;
+const run = async () => {
+    try {
+        await client.connect();
+        const productsCollection = client.db('warehouse').collection('warehouseproduct');
 
-            app.get('/warehouseproducts', async (req, res) => {
-                const query = {};
-                const cursor = productsCollection.find(query)
-                const products = await cursor.toArray();
-                res.send(products)
-            })
-           
-            app.get('/warehouseproducts/:id', async(req, res) => {
-                const id = req.params.id;
-                const query = {_id:ObjectId(id)}
-                const singleProduct = await productsCollection.findOne(query)
-                res.send(singleProduct)
-            })
-            app.put('/warehouseproducts/:id', async(req, res) => {
-                const id = req.params.id ;
-                const filter = {_id:ObjectId(id)}
-                const quantity = req.body.newQuantity;
-                const sold = req.body.newSold;
-                const options = { upsert : true}
-                const updateDoc = {
-                    $set:{
-                        quantity : quantity - 1  ,
-                        sold : sold + 1
-                    }
+        app.get('/warehouseproducts', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+        app.get('/warehouseproducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const singleProduct = await productsCollection.findOne(query)
+            res.send(singleProduct)
+        })
+        app.put('/warehouseproducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const quantity = req.body.newQuantity;
+            const sold = req.body.newSold;
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    quantity: quantity - 1,
+                    sold: sold + 1
                 }
-                const final =  await productsCollection.updateOne(filter, updateDoc, options)
-                res.send(final)
-            })
+            }
+            const final = await productsCollection.updateOne(filter, updateDoc, options)
+            res.send(final)
+        })
 
-            app.post('/warehouseproducts', async(req, res) => {
-                const newProduct = req.body;
-                console.log(newProduct);
-                const insertItem = await productsCollection.insertOne(newProduct)
-                res.send(insertItem)
-            })
-             app.get('/addedproducts', async (req, res) => {
-                const email = req.query.email;
-                const query = {email};
-                const cursor = productsCollection.find(query)
-                const products = await cursor.toArray();
-                res.send(products)
-            })
-            app.delete('/warehouseproducts/:id', async (req, res) => {
-                const id = req.params.id;
-                const query = {_id:ObjectId(id)}
-                const deleteProduct = await productsCollection.deleteOne(query)
-                res.send(deleteProduct)
-            })
+        app.post('/warehouseproducts', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const insertItem = await productsCollection.insertOne(newProduct)
+            res.send(insertItem)
+        })
+        app.get('/addedproducts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+        app.delete('/warehouseproducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const deleteProduct = await productsCollection.deleteOne(query)
+            res.send(deleteProduct)
+        })
 
-        }
-        finally{}
     }
-    run().catch(console.dir);
-
+    finally { }
+}
+run().catch(console.dir);
+//Initial
 app.get('/', (req, res) => {
     res.send('Runnign')
-}) 
+})
 app.listen(port, () => {
     console.log('Listening from port : ', port);
 })
